33d7dd94f499a62f5adad1e4b1ecaeb09cd06502
Kara Cavanaugh <karacav@msn.com>
2018-02-14 22:21:18 -0500

    finished

--- scripts.js
+++ scripts.js
@@ -12,4 +12,21 @@ function debounce(func, wait = 20, immediate = true) {
     timeout = setTimeout(later, wait);
     if (callNow) func.apply(context, args);
   };
+  //
 }
+const sliderImages = document.querySelectorAll('.slide-in');
+function checkSlide(e) {
+  sliderImages.forEach(image => {
+    const slideInAt = window.scrollY + window.innerHeight - image.height / 2;
+    const imageBottom = image.offsetTop + image.height;
+    const isHalfShown = slideInAt > image.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', checkSlide);
